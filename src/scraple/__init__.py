6a528f91ac5918550fe4bb8d9afb68627507b86c
max-efort <134115482+max-efort@users.noreply.github.com>
2023-06-16 22:35:28 +0800

    Create __init__.py

---
+++ src/scraple/__init__.py
@@ -0,0 +1,46 @@
+"""
+=============================
+scraple - Simple Scraping Library
+=============================
+
+Scraple is a Python library designed to simplify the process of web scraping.
+This library provide easy scraping and easy searching for selectors.
+
+Overview
+---------
+
+Simprap provides a simple way to extract data from web pages by defining simple rules
+for extraction. It also offers a convenient mechanism for automatically finding
+selectors using only string present in the page.
+
+Main Features
+---------
+
+- **SimpleExtractor**: A class that enables rule-based extraction of data from web pages,
+  making the actual scraping easy.
+
+- **Rules**: A class that allows the automatic discovery of selectors using string.
+  It simplifies the process of defining and applying extraction rules.
+
+Exception
+--------------
+
+Simprap includes the following error classes:
+
+- **ParsingError**: Raised when an unexpected error occurs during page parsing.
+- **SearchError**: Raised when an unexpected error occurs while searching
+  for a string in a page.
+- **ExtractError**: Raised when an unexpected error occurs during data extraction
+  from a page.
+
+Dependencies
+------------
+- **beautifulsoup4** (version 4.x.x): A library for parsing HTML and XML documents.
+
+
+For more information visit https://github.com/max-efort/.
+
+"""
+
+from extract import SimpleExtractor, Rules
+from error import *
